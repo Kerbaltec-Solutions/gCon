6433cfad359aee485bf189ad62f24164f414f6b3
Björn Schnabel <61379284+Kerbaltec-Solutions@users.noreply.github.com>
2025-04-08 16:31:32 +0200

    Cleanup gesture-control.py
    Clean code and comment the code

--- gesture-control.py
+++ gesture-control.py
@@ -8,18 +8,22 @@ from mediapipe.framework.formats import landmark_pb2
 import pyautogui
 import numpy as np
 
+# Disable pyautogui failsafe, to avoid error when cursor touches screen corners
 pyautogui.FAILSAFE = False
 
+# get screen dimensions
 w=pyautogui.size().width
 h=pyautogui.size().height
 
+# prepare matixes for least squares interpolation for cursor position smoothing
 A = np.array([[1,i]for i in range(-10,0)])
 print(A)
 Ap = np.linalg.pinv(A)
 y = np.array([1,-1])
 
+# map hand position to screen position
 def translate(value, leftMin, leftMax, rightMin, rightMax):
-    # Figure out how 'wide' each range is
+    # Figure out how wide each range is
     leftSpan = leftMax - leftMin
     rightSpan = rightMax - rightMin
 
@@ -33,6 +37,8 @@ def translate(value, leftMin, leftMax, rightMin, rightMax):
     # Convert the 0-1 range into a value in the right range.
     return v
 
+# Statemachine to store the currently performed action
+# Will rewerite this as switch statement later
 class statemachine:
   move = True
 
@@ -85,18 +91,15 @@ class statemachine:
         self.lasty = posy
     print("State:", self.state)
 
-mp_hands = mp.solutions.hands
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-
-width, height, fps = int(640), int(460), 30
-
+# Initialize google mediapipe algorithm
 base_options = python.BaseOptions(model_asset_path='gesture_recognizer.task')
 options = vision.GestureRecognizerOptions(base_options=base_options)
 recognizer = vision.GestureRecognizer.create_from_options(options)
 
+# Initialize statemachine
 sm = statemachine()
 
+# Initialize buffer for hand positions for smoothing
 center_history = []
 
 def get_frames():
@@ -106,74 +109,60 @@ def get_frames():
     ret, frame = video_capture.read()
 
     if(ret):
-      # Resize frame of video to 1/4 size for faster face recognition processing
+      # Resize frame of video to 1/4 size for faster face recognition processing and preprocess
       small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
       small_frame = cv2.flip(small_frame, 1)
       rgb_img = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
-
-      #frame = np.zeros((height, width, 3), np.uint8)
-      #frame=cv2.flip(frame, 1)
-
-      # Process face
       mpImg=mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_img)
+
+      # recognize Hands
       recognition_result = recognizer.recognize(mpImg)
 
+      # if no hands, skip.
       if(len(recognition_result.gestures) == 0):
-        #print('No gesture detected')
         continue
 
-      top_gesture = recognition_result.gestures[0][0]
-      hand_landmarks = recognition_result.hand_landmarks
+      # get gesture and hand landmark positions from result
+      gestures = recognition_result.gestures[0][0]
+      hand_landmarks_list = recognition_result.hand_landmarks
 
-      gestures = top_gesture
-      hand_landmarks_list = hand_landmarks
-
-      #annotated_image = image.copy()
-
+      # for all hands
       for hand_landmarks in hand_landmarks_list:
+        # normalize landmark positions
         hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
         hand_landmarks_proto.landmark.extend([
           landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
         ])
 
+        # get position of base of index finger, choosen because intuitive and relatively stable
         center = hand_landmarks_proto.landmark[5]
         print('Gesture:', gestures.category_name)
-        print('Score:', gestures.score)
-        print('Center:', center)
+        #print('Score:', gestures.score)
+        #print('Center:', center)
 
-        # Rolling average for center with length 4
-
+        # add position to history
         center_history.append(center)
         if len(center_history) > 10:
           center_history.pop(0)
+          # compute linear least squares interpolation of last 10 data points, provides very responsive noise reduction
           b = np.array([[c.x, c.y, c.z] for c in center_history])
           x = np.dot(Ap,b)
           avg_center = np.dot(y,x)
-          #print(avg_center)
+          print('Center:', avg_center)
           center.x = avg_center[0]
           center.y = avg_center[1]
           center.z = avg_center[2]
 
-        '''weights = [2*(i+1) for i in range(len(get_frames.center_history))]
-        total_weight = sum(weights)
-        avg_center = landmark_pb2.NormalizedLandmark(
-          x=sum(c.x * w for c, w in zip(get_frames.center_history, weights)) / total_weight,
-          y=sum(c.y * w for c, w in zip(get_frames.center_history, weights)) / total_weight,
-          z=sum(c.z * w for c, w in zip(get_frames.center_history, weights)) / total_weight
-        )'''
-
-        #center = avg_center
-
-        if(gestures.score > 0.51):
+        # if hand is likely and within bounding box for input
+        if(gestures.score > 0.51 and center.x > 0.55 and center.x < 0.9 and center.y > 0.25 and center.y < 0.85):
+          # update statemachine and move cursor
           sm.update(gestures.category_name,center.y)
-          if(center.x > 0.55 and center.x < 0.9 and center.y > 0.25 and center.y < 0.85 and sm.move):
+          if(sm.move):
             pyautogui.moveTo(translate(center.x,0.6,0.85,0,w), translate(center.y,0.3,0.8,0,h), duration=0.0, _pause=False)
 
+  # free resources
   video_capture.release()
   cv2.destroyAllWindows()
-
-#.6 .9
-#.3 .8
 
 if __name__ == '__main__':
   get_frames()
